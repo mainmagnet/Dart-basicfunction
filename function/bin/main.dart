a69ca91df7837f4cbe908653cd69c02fc8ff6347
Omer Akkus <omeristnow@gmail.com>
2020-10-17 11:59:18 +0300

    first commit

---
+++ function/bin/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  String message = 'Hello world ';
+  int bornyear = 1988;
+  double rate = 1.8;
+  bool torf = false;
+
+  print(message);
+  print('You Born Year == > ' + bornyear.toString());
+  print('Rate == 1.7 ? ' + torf.toString() + ' Rate == > ' + rate.toString());
+}
